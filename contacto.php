9bec877eec37f98d33415cd520bcb001906d6a0b
maximoamore2001 <maxiamorerc88@gmail.com>
2024-01-18 12:10:16 -0300

    cambios

--- contacto.php
+++ contacto.php
@@ -14,7 +14,7 @@ $config = obtenerConfiguracion();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SAWPI - Inmobiliaria</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="./css/estilo.css">
 </head>
 
 <body class="page-contacto">
--- propiedades.php
+++ propiedades.php
@@ -21,7 +21,7 @@ $result_propiedades = cargarPropiedades($limInferior);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SAWPI - Inmobiliaria</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="./css/estilo.css">
 
     <script>
         function cargarMasPropiedades(str) {
